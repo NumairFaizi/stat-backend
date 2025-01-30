7dee2ada3a9b79bcf0291bdd7dabf63da2fc81c1
NumairFaizi <numairfaizee@gmail.com>
2025-01-30 17:57:00 +0530

    asdsd

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(errorHandler);
 app.use('/api/product',require('./routes/productRoutes'));
+app.use('/api/users' ,require('./routes/userRoutes'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
