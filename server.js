4cb5821e7014421900c47b4d72975271e45db948
NumairFaizi <numairfaizee@gmail.com>
2025-02-07 20:00:36 +0530

    asd

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
@@ -8,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(errorHandler);
 app.use('/api/product',require('./routes/productRoutes'));
 app.use('/api/users' ,require('./routes/userRoutes'));
