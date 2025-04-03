b51a7838e5351b4680146569c04874505fc24268
MohammedHassan07 <mhassancs260@gmail.com>
2025-04-03 12:58:20 +0530

    FEAT: Billing data

--- controllers/billingController.js
+++ controllers/billingController.js
@@ -5,19 +5,44 @@ const billingModel = require("../models/billingModel");
 const asyncHandler = require("express-async-handler");
 
 //@access private
-const addBill = asyncHandler(async(req, res) => {
-    
+const addBill = asyncHandler(async (req, res) => {
+
     const bill = req.body
     // console.log(bill)
 
-    const newBill =  new billingModel(bill)
+    const newBill = new billingModel(bill)
 
     const savedBill = await newBill.save()
-    res.status(200).json({message: 'Bill added successfully'})
+    res.status(200).json({ message: 'Bill added successfully' })
 
 });
 
+const billBySearchString = asyncHandler(async (req, res) => {
+
+    const searchString = req.params.searchString
+    console.log(searchString)
+
+    res.status(200).json({ message: 'Show bill' })
+})
+
+const getBills = asyncHandler(async (req, res) => {
+
+    // console.log(req)
+
+    const billingData = await billingModel.find()
+
+    if (!billingData) {
+
+        res.status(404).json({ message: "No Data Found" });
+        throw new Error("Billing Data not Found");
+    }
+
+    res.status(200).json({ message: 'Show bills', billingData })
+})
+
 
 module.exports = {
-    addBill
+    addBill,
+    billBySearchString,
+    getBills
 }
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { 
-   addBill
+const {
+   addBill,
+   billBySearchString,
+   getBills,
 } = require("../controllers/billingController");
 const validateToken = require("../middleware/validateTokenHandler");
 
@@ -8,5 +10,9 @@ const router = express.Router();
 
 
 router.post("/add-bill", validateToken, addBill);
+
+
+router.get("/bill-by-search-string/:searchString", validateToken, billBySearchString)
+   .get('/bills', getBills)
 
 module.exports = router;
